1e80fff2c3046d1b41fcfe6f5d2218a555109bf5
GDComfort <103566706+GDComfort@users.noreply.github.com>
2023-02-27 16:42:00 -0700

    Add files via upload

--- Problem1.py
+++ Problem1.py
@@ -2,26 +2,29 @@
 #Done by Guillaume Comfort.
 
 class Scheduling:
-    #self.data = [9, 8, 6, 2, 3, 4, 5, 7, 10, 1] Data Set
-    #self.num = 10 Number of processes to complete.
+    #scheddata = [9, 8, 6, 2, 3, 4, 5, 7, 10, 1] Data Set
+    #schednum = len(selfdata) Number of processes to complete.
 
 # FCFS
     def FirstCome():
-        selfdata = [9, 8, 6, 2, 3, 4, 5, 7, 10, 1]
+        scheddata = [9, 8, 6, 2, 3, 4, 5, 7, 10, 1]
+        schednum = len(scheddata)
         WaitingTime = 0.00
         Totalwait = 0.00
         AvgWait = 0.00
         TAT = 0.00
 
-        for i in range(len(selfdata)):
-            num = selfdata[i]
+        for i in range(schednum):
+            num = scheddata[i]
             while num >= 0:
                 print(num)
                 num -= 1
                 Totalwait += 1
                 WaitingTime += 1
-            print(WaitingTime - 1)
+            print("Waiting Time:"+str(WaitingTime - 1))
             WaitingTime = 0.00
-        print(Totalwait - len(selfdata))
+        Avgwait = (Totalwait / schednum)
+        print("Total Wait Time:"+str(Totalwait - schednum))
+        print("Average Wait Time:"+str(Avgwait))
 
     FirstCome()
